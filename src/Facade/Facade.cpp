bd9ef22c53b955ea6b2597583ead318ae284c0c6
XiaoRong <cjohnxiao@gmail.com>
2021-03-15 09:26:09 +0800

    main工程配置为x64的release版编译通过

--- src/Facade/Facade.cpp
+++ src/Facade/Facade.cpp
@@ -5,7 +5,6 @@ Facade::Facade():
     m_camera_grabber_(GetModuleDirectory() + PathSeparator() + "CameraGrabber"),
     m_plc_agent_(GetModuleDirectory() + PathSeparator() + "PlcAgent")
 {
-	GlobalLogger::Record("Main", LOG_LEVEL::TRACK, "VisionInspection Begin");
     //m_plc_agent_.Init();
     m_camera_grabber_.Init();       
 }
@@ -18,7 +17,7 @@ void Facade::Run()
 {
     //PlcAgentPtr plc_agent = PlcAgent();
     //std::cout << plc_agent->ShotTriggered() << endl;
-	
+	GlobalLogger::Record("Main", LOG_LEVEL::TRACK, "VisionInspection Begin");
 	auto camera_grabber = CameraGrabber();
 	camera_grabber->InitCamera();
 }
